1aa646606ca51c1e3b4019815061cf745a1bc66b
Esteban Alvarado <estalvgs1999@gmail.com>
2020-04-06 20:39:34 -0600

    Estructura inicial del proyecto
    * Directorio frontend con src,img, sounds.
    * Directorio backend con src y data.

---
+++ Frontend/src/js/app.js
@@ -0,0 +1,11 @@
+/* -------------------------------------------
+ * File: app.js
+ * Develop by: @smidvarg
+ * Description: controls events and connect w/
+ * backend.
+ *
+ * version 0.1
+ * Last edit : 6/4/2020 [20:30]
+ *
+ * A project by av_software 
+ * ------------------------------------------ */
