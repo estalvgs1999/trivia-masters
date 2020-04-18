d68a153517bae3bc89515e402f5350b20c0e811b
Esteban Alvarado <estalvgs1999@gmail.com>
2020-04-17 22:01:44 -0600

    Comentario de bienvenida en run.py

--- Backend/src/run.py
+++ Backend/src/run.py
@@ -1,3 +1,20 @@
+# -----------------------------
+# File: run.py
+# Project: Trivia
+# dev by: @estalvgs1999
+#
+# Description: This is the launcher 
+# script, simple because its a game 
+# and it's not supposed to be difficult 
+# to use. Just run it.
+# Well i hope you enjoy playing Trivia!
+#
+# version 0.1
+# last edited: 17/4/20 [23:00]
+#
+# an av_software project
+#-------------------------------
+
 import restapi
 
 if __name__ == "__main__":
